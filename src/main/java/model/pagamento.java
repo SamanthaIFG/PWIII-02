05f77d26330aa96635e9655fb4c0005c301c7606
20221080030038 <samanthamariaalvesdesouza@gmail.com>
2024-08-26 15:48:25 -0300

    commint01

---
+++ src/main/java/model/pagamento.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.Date;
+
+public class pagamento {
+    private long id;
+
+    @Override
+    public String toString() {
+        return "pagamento{" +
+                "id=" + id +
+                ", data=" + data +
+                ", valor=" + valor +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    private Date data;
+    private float valor;
+
+}
