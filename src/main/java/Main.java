19939a0a5dc2787d2a9f814621431356a3a0d6ee
20221080030038 <samanthamariaalvesdesouza@gmail.com>
2024-08-26 15:48:51 -0300

    commint01

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import model.pagamento;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        pagamento pg = new pagamento();
+        pg.setId(11);
+        pg.setValor(500);
+        pg.setData(new Date());
+        System.out.println(pg);
+    }
+}
